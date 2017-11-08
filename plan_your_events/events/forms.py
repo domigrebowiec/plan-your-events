8c94acd2d8741ceed32edfcd1f4c1173545e9ca2
Dominika Grebowiec <dominika.grebowiec@gmail.com>
2017-11-08 23:15:54 +0100

    Styles for event form

--- plan_your_events/events/forms.py
+++ plan_your_events/events/forms.py
@@ -3,6 +3,6 @@ from django.utils.translation import ugettext_lazy as _
 
 class EventForm(forms.Form):
   name = forms.CharField(label=_('Event name'), max_length=100)
-  start_time = forms.DateTimeField(label='Start time')
-  end_time = forms.DateTimeField(label='End time')
+  start_time = forms.DateTimeField(label='Start time', widget=forms.widgets.DateInput(attrs={'type': 'datetime-local'}))
+  end_time = forms.DateTimeField(label='End time', widget=forms.widgets.DateInput(attrs={'type': 'datetime-local'}))
   description = forms.CharField(widget=forms.Textarea)
