06e0165fa5941d0bb8096568452b44a53f5f0c38
Dominika Grebowiec <dominika.grebowiec@gmail.com>
2017-11-09 00:01:24 +0100

    Event detail view enhanced.

--- plan_your_events/events/forms.py
+++ plan_your_events/events/forms.py
@@ -2,7 +2,12 @@ from django import forms
 from django.utils.translation import ugettext_lazy as _
 
 class EventForm(forms.Form):
-  name = forms.CharField(label=_('Event name'), max_length=100)
+  name = forms.CharField(label=_('Event name'))
   start_time = forms.DateTimeField(label='Start time', widget=forms.widgets.DateInput(attrs={'type': 'datetime-local'}))
   end_time = forms.DateTimeField(label='End time', widget=forms.widgets.DateInput(attrs={'type': 'datetime-local'}))
   description = forms.CharField(widget=forms.Textarea)
+
+class PersonForm(forms.Form):
+  first_name = forms.CharField(label=_('First name'))
+  last_name = forms.CharField(label=_('Last name'))
+  email = forms.EmailField()
